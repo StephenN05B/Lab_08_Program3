15379cc36c2c058d4324e817cc7c9ce6dd9dfc77
steph <stephenbenny050@gmail.com>
2024-10-24 17:39:22 -0400

    Task 1

--- src/DevTest.java
+++ src/DevTest.java
@@ -42,41 +42,6 @@ public class DevTest
         return retVal;
     }
 
-    /**
-     * get an int value from the user at the console with no constraint
-     *
-     *
-     * @param pipe a Scanner used to get the input
-     * @param prompt the prompt tells the user what to enter
-     * @return an int of any value
-     */
-    public static int getInt(Scanner pipe, String prompt)
-    {
-        int retVal = 0;
-        boolean done = false;
-        String trash = "";
-
-        do
-        {
-            System.out.print(prompt + ": ");
-            if(pipe.hasNextInt())
-            {
-                retVal = pipe.nextInt();
-                pipe.nextLine();
-                done = true;
-            }
-            else
-            {
-                trash = pipe.nextLine();
-                System.out.println("You must enter a valid integer not " + trash);
-            }
-
-
-
-        }while(!done);
-
-        return retVal;
-    }
 
 
 }
